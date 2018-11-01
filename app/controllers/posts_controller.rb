0dea71a468e4dc3a6fe422ea621305e81678b868
yoga permana tanaya <tanayayoga@gmail.com>
2018-11-01 12:36:53 +0800

    finalisasi crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,8 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+
+    # buat create
     def create
         #render plain: params[:post].inspect
         @post = Post.new(post_params)
@@ -14,11 +16,33 @@ class PostsController < ApplicationController
             render 'new' 
         end
     end
+    # ambil id 
     private def post_params
         params.require(:post).permit(:title, :body )
     end
+
+    # get
     def show
         @post = Post.find(params[:id]);
+    end
 
+    # buat edit
+    def edit
+        @post = Post.find(params[:id]);
+    end
+    def update
+        @post = Post.find(params[:id]);
+        if(@post.update(post_params))
+            redirect_to @post
+        else
+            render 'edit' 
+        end
+    end
+
+    # delete
+    def destroy
+        @post = Post.find(params[:id]);
+        @post.destroy
+        redirect_to posts_path
     end
 end
